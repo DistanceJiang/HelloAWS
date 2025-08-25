8edea0c4d92eb5128d19091acb7e34913b0bc698
Rainman <distancein1997@gmail.com>
2025-08-25 18:30:36 +0800

    added log for user service

--- src/main/java/com/rainman/helloaws/service/UserService.java
+++ src/main/java/com/rainman/helloaws/service/UserService.java
@@ -2,6 +2,8 @@ package com.rainman.helloaws.service;
 
 import com.rainman.helloaws.entity.User;
 import com.rainman.helloaws.repo.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,10 +11,27 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        log.debug("Creating user: {}", user);
+        User savedUser = userRepository.save(user);
+        log.info("User created with id: {}", savedUser.getId());
+        return savedUser;
+    }
+
+    public User updateUser(Long id, User user) {
+        return getUser(id).map(u -> {
+            u.setName(user.getName());
+            u.setEmail(user.getEmail());
+            return userRepository.save(u);
+        }).orElse(null);
     }
 
     public List<User> getAllUsers() {
@@ -20,24 +39,14 @@ public class UserService {
     }
 
     public Optional<User> getUser(Long id) {
-        return userRepository.findById(id);
-    }
-
-    public User createUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public User updateUser(Long id, User user) {
-        return userRepository.findById(id)
-                .map(u -> {
-                    u.setName(user.getName());
-                    u.setEmail(user.getEmail());
-                    return userRepository.save(u);
-                })
-                .orElse(null);
+        log.debug("Searching user with id: {}", id);
+        Optional<User> user = userRepository.findById(id);
+        user.ifPresentOrElse(u -> log.info("User found: {}", u), () -> log.warn("User not found for id: {}", id));
+        return user;
     }
 
     public void deleteUser(Long id) {
+        log.debug("Deleting user with id: {}", id);
         userRepository.deleteById(id);
     }
 }
